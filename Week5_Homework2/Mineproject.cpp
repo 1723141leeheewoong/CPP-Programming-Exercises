0efc5d26a4a9484505b2ffd2f45cb59612352266
1723141leeheewoong <102776664+1723141leeheewoong@users.noreply.github.com>
2022-04-19 13:15:23 +0900

    Add files via upload

---
+++ Week5_Homework2/Mineproject.cpp
@@ -0,0 +1,12 @@
+﻿#include "MineSweeper.h"
+int main()
+{
+	int width, height, total;
+	printf(" <지뢰찾기>\n");
+	printf(" 지뢰맵의 크기 입력(가로 세로) : ");
+	scanf_s("%d%d", &width, &height);
+	printf(" 매설할 총 지뢰의 개수 입력 : ");
+	scanf_s("%d", &total);
+
+	playMineSweeper(width, height, total);
+}
