819d47718480cdca89f0df601d5cac24b02e9673
1723141leeheewoong <102776664+1723141leeheewoong@users.noreply.github.com>
2022-06-03 11:39:23 +0900

    Add files via upload

---
+++ Week11_Homework_2/monmon.cpp
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <iostream>
+#include <iomanip>
+#include "TimeHMS.h"
+
+int main()
+{
+	TimeHMS	t1(10000), t2, t3(11, 22, 33), t4, t5, t6;
+
+	cout << "t2: ";
+	cin >> t2;
+
+	cout << "t1      = " << t1;
+	cout << "t2      = " << t2;
+	cout << "t3      = " << t3;
+	cout << "t1+t2   = " << t1 + t2;
+	cout << "t1-t2   = " << t1 - t2;
+	cout << "++t1    = " << ++t1;
+	cout << "--t1    = " << --t1;
+	cout << "t1++    = " << t1++;
+	cout << "t1--    = " << t1--;
+
+	cout << "t1[0~2] = " << t1[0] << " " << t1[1] << " " << t1[2] << endl;
+	cout << "t2[sec] = " << (int)t2 << endl;
+	cout << "t2[hour]= " << (double)t2 << endl;
+	cout << "2*t2    = " << 2 * t2;
+}
+
+
